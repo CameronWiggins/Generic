6ee558ae0b52287377bad34314e3249a420082bc
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-09-13 10:42:38 -0500

    testing

---
+++ src/Stack.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+public class Stack<T> 
+{
+    ArrayList<T> stack;
+
+    public Stack()
+    {
+        stack = new ArrayList<T>();
+    }
+
+    public void push(T top)
+    {
+        stack.add(top);
+    }
+
+    public T pop()
+    {
+        return stack.remove(stack.size() - 1);
+    }
+
+    public T peek()
+    {
+        return stack.get(stack.size() - 1);
+    }
+
+    public boolean isEmpty()
+    {
+        if (stack.size() == 0)
+        return true;
+        else
+        return false;
+    }
+}
