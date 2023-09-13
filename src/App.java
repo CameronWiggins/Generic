9a27d6fb32295048a242b69dc30be06e00b96bcd
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-09-13 10:40:48 -0500

    methods tested

--- src/App.java
+++ src/App.java
@@ -1,5 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Stack<Integer> stack1 = new Stack<Integer>();
+        System.out.println(stack1.isEmpty());
+        stack1.push(23);
+        System.out.println(stack1.isEmpty());
+        System.out.println(stack1.peek());
+        System.out.println(stack1.pop());
     }
 }
