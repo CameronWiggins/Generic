782a2e7d78c21b44323aab69a78218ae9b2c5540
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-09-21 10:07:36 -0500

    testing postfix

--- src/App.java
+++ src/App.java
@@ -1,11 +1,38 @@
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Stack<Integer> stack1 = new Stack<Integer>();
+        /*Stack<Integer> stack1 = new Stack<Integer>();
         System.out.println(stack1.isEmpty());
         stack1.push(23);
         System.out.println(stack1.isEmpty());
         System.out.println(stack1.peek());
         System.out.println(stack1.pop());
+        */
     }
+
+    public static int postFix(String expression)
+    {
+        Stack<String> solver = new Stack<String>();
+        for (int i = 0; i < expression.length(); i++)
+        {
+            String newTop = expression.substring(i,i + 1);
+            if (newTop.equals("+"))
+            {
+                int numOne = Integer.parseInt(solver.pop());
+                int numTwo = Integer.parseInt(solver.pop());
+                solver.push(numOne + numTwo + "");
+            }
+            else if (newTop.equals("*"))
+            {
+                int numOne = Integer.parseInt(solver.pop());
+                int numTwo = Integer.parseInt(solver.pop());
+                solver.push(numOne * numTwo + "");
+            }
+            else
+            {
+                solver.push(newTop);
+            }
+        }
+        return Integer.parseInt(solver.pop());
+    } 
 }
